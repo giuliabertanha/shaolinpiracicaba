b503e17a0d3b96d3c6e00b54c5e93e7fcfedf65d
Giulia <giuliabertanha.tech@gmail.com>
2025-11-06 17:28:35 -0300

    Inclusão de usuários

--- incluir_aluno.php
+++ incluir_aluno.php
@@ -8,7 +8,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
-}
+} 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $usuario = $_POST['usuario'];
